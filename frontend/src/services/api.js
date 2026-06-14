d8ebe235efb9c9024202404854b3830dde712813
Riya3024 <riyapanditstm@gmail.com>
2026-06-14 22:08:42 +0530

    Add production API configuration

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
+
 const API = axios.create({
-  baseURL: "http://127.0.0.1:8000",
-  headers: {
-    "Content-Type": "application/json",
-  },
+  baseURL: import.meta.env.VITE_API_URL
 });
+
+
 
 // =========================
 // VEHICLES
